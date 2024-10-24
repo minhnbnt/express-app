518042d740f4ac0c33d97b2a183f9f1231471975
minhnbnt <minhnbnt@gmail.com>
2024-10-24 21:56:07 +0700

    initial commit

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/get.php", async (_, res) => {
+  const { data } = await axios.get(
+    "http://sharing.gotdns.ch:8091/thapcam.php",
+    { responseType: "text" },
+  );
+
+  res.type("text/plain").send("#EXTM3U\n" + data);
+});
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+export default app;
